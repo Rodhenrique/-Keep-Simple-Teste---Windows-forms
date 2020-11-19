464a364f854b20339c831250d3b58bf17a33c7e5
Rodhenrique <henriquesilvasenai@gmail.com>
2020-11-19 18:57:25 -0300

    finish

---
+++ Henrique/Views/Home.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Henrique
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.TbData = new System.Windows.Forms.DateTimePicker();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TbDistancia = new System.Windows.Forms.TextBox();
+            this.TbCusto = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabela = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TbNivel = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(544, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(346, 34);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Excluir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.excluir_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Incluir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.incluir_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.TbData);
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.TbNivel);
+            this.panel2.Controls.Add(this.TbDistancia);
+            this.panel2.Controls.Add(this.TbCusto);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Location = new System.Drawing.Point(544, 73);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(345, 415);
+            this.panel2.TabIndex = 1;
+            // 
+            // TbData
+            // 
+            this.TbData.Enabled = false;
+            this.TbData.Location = new System.Drawing.Point(95, 23);
+            this.TbData.Name = "TbData";
+            this.TbData.Size = new System.Drawing.Size(222, 20);
+            this.TbData.TabIndex = 18;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.checkBox2.Location = new System.Drawing.Point(97, 161);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(58, 24);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.Text = "Não";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Click += new System.EventHandler(this.checkBox2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.CausesValidation = false;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.checkBox1.Location = new System.Drawing.Point(200, 161);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 24);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "Sim";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label6.Location = new System.Drawing.Point(10, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 22);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Captura";
+            // 
+            // TbDistancia
+            // 
+            this.TbDistancia.Enabled = false;
+            this.TbDistancia.Location = new System.Drawing.Point(95, 114);
+            this.TbDistancia.Name = "TbDistancia";
+            this.TbDistancia.Size = new System.Drawing.Size(222, 20);
+            this.TbDistancia.TabIndex = 9;
+            // 
+            // TbCusto
+            // 
+            this.TbCusto.Enabled = false;
+            this.TbCusto.Location = new System.Drawing.Point(95, 72);
+            this.TbCusto.Name = "TbCusto";
+            this.TbCusto.Size = new System.Drawing.Size(222, 20);
+            this.TbCusto.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label5.Location = new System.Drawing.Point(13, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 22);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Nivel dor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label4.Location = new System.Drawing.Point(34, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 22);
+            this.label4.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label3.Location = new System.Drawing.Point(10, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Distência";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label2.Location = new System.Drawing.Point(10, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Custo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label1.Location = new System.Drawing.Point(10, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Data";
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(185, 310);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 29);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Cancelar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.cancelar_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(66, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 29);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Salvar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.salvar_Click);
+            // 
+            // tabela
+            // 
+            this.tabela.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.tabela.HideSelection = false;
+            this.tabela.Location = new System.Drawing.Point(61, 21);
+            this.tabela.Name = "tabela";
+            this.tabela.Size = new System.Drawing.Size(407, 467);
+            this.tabela.TabIndex = 3;
+            this.tabela.UseCompatibleStateImageBehavior = false;
+            this.tabela.View = System.Windows.Forms.View.Details;
+            this.tabela.SelectedIndexChanged += new System.EventHandler(this.tabela_SelectedIndexChanged_1);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Data";
+            this.columnHeader1.Width = 130;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Captura";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 137;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Nivel de dor";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 136;
+            // 
+            // TbNivel
+            // 
+            this.TbNivel.Enabled = false;
+            this.TbNivel.Location = new System.Drawing.Point(95, 209);
+            this.TbNivel.Name = "TbNivel";
+            this.TbNivel.Size = new System.Drawing.Size(222, 20);
+            this.TbNivel.TabIndex = 10;
+            this.TbNivel.TextChanged += new System.EventHandler(this.TbNivel_TextChanged_1);
+            this.TbNivel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TbNivel_KeyPress);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 505);
+            this.Controls.Add(this.tabela);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Home";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox TbDistancia;
+        private System.Windows.Forms.TextBox TbCusto;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DateTimePicker TbData;
+        private System.Windows.Forms.ListView tabela;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox TbNivel;
+    }
+}
+
---
+++ Henrique/Views/Home.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Henrique
+{
+    public partial class Home : Form
+    {
+        private const string PATH = "acme.sqlite";
+        private bool status = true;
+        private bool excluirItem = false;
+        private static SQLiteConnection connection;
+        public static List<DataRow> listItens { get; set; }
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private static SQLiteConnection DbConnection()
+        {
+            connection = new SQLiteConnection("Data Source=acme.sqlite; Version=3;");
+            connection.Open();
+            return connection;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "ACME FLIGHT MANAGER";
+
+
+            //CRIAR UM ARQUIVO SQLITE CASO NÃO EXISTAM
+            if (!File.Exists(PATH)) {
+                SQLiteConnection.CreateFile(PATH);
+                using (var cmd = DbConnection().CreateCommand())
+                {
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS TB_VOO(ID_VOO INTEGER PRIMARY KEY, DATA_VOO Datetime,CUSTO numeric (10,2), DISTANCIA Int,CAPTURA Char(1), NIVEL_DOR Int)";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            Listar();
+        }
+        //MÉTODO GET PARA LISTAR TODOS REGISTROS DO BANCO DE DADOS 
+        public void Listar()
+        {
+            SQLiteDataAdapter da = null;
+            DataSet ds = new DataSet();
+            List<DataRow> objs = new List<DataRow>();
+
+            try
+            {
+                using (var cmd = DbConnection().CreateCommand())
+                {
+                    //DATA_VOO,CAPTURA,NIVEL_DOR
+                    cmd.CommandText = "SELECT * FROM TB_VOO";
+                    da = new SQLiteDataAdapter(cmd.CommandText, DbConnection());
+                    da.Fill(ds);
+
+                    foreach (DataRow dtrow in ds.Tables[0].Rows)
+                    {
+                        objs.Add(dtrow);
+                        string[] row = { dtrow.ItemArray[1].ToString(), dtrow.ItemArray[4].ToString(), dtrow.ItemArray[5].ToString() };
+                        var listViewItem = new ListViewItem(row);
+                        tabela.Items.Add(listViewItem);
+                    }
+                    listItens = objs;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        //MÉTODO POST DE REGISTRO ADICONANDO NO ARQUIVO SQLITE
+        private void salvar_Click(object sender, EventArgs e)
+        {
+            if (verifyCampos() == true)
+            {
+            if (status == true)
+            {
+                try
+                {
+                    using (var cmd = DbConnection().CreateCommand())
+                    {
+                        cmd.CommandText = "INSERT INTO TB_VOO(DATA_VOO, CUSTO,DISTANCIA,CAPTURA,NIVEL_DOR) values (@data, @custo, @distancia,@captura,@nivel)";
+                        cmd.Parameters.AddWithValue("@data", DateTime.Parse(TbData.Text));
+                        cmd.Parameters.AddWithValue("@custo", TbCusto.Text);
+                        cmd.Parameters.AddWithValue("@distancia", TbDistancia.Text);
+                        cmd.Parameters.AddWithValue("@captura", (checkBox2.Checked) ? "N" : "S");
+                        cmd.Parameters.AddWithValue("@nivel", TbNivel.Text);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            else
+            {
+                try
+                {
+                    int select = tabela.SelectedItems[0].Index;
+                    var item = listItens[select].ItemArray;
+                    int id = int.Parse(item[0].ToString());
+                    using (var cmd = DbConnection().CreateCommand())
+                    {
+                        cmd.CommandText = "UPDATE TB_VOO SET DATA_VOO = @data, CUSTO = @custo ,DISTANCIA = @distancia,CAPTURA = @captura,NIVEL_DOR = @nivel WHERE ID_VOO=@Id";
+                        cmd.Parameters.AddWithValue("@Id", id);
+                        cmd.Parameters.AddWithValue("@data", DateTime.Parse(TbData.Text));
+                        cmd.Parameters.AddWithValue("@custo", TbCusto.Text);
+                        cmd.Parameters.AddWithValue("@distancia", TbDistancia.Text);
+                        cmd.Parameters.AddWithValue("@captura", (checkBox2.Checked) ? "N" : "S");
+                        cmd.Parameters.AddWithValue("@nivel", TbNivel.Text);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+                excluirItem = false;
+                limparCampos();
+                tabela.Items.Clear();
+                habilitar(false);
+                Listar();
+            }
+            else
+            {
+                MessageBox.Show("Preenchar os campos!!!");
+            }
+        }
+
+        //VERIFICAR SE OS CAMPOS ESTÃO VAZIOS PARA FAZER UMA REQUEST
+        private bool verifyCampos()
+        {
+            DateTime hora = DateTime.Now;
+            if (TbData.Text == null || TbData.Text == "" || Convert.ToDateTime(TbData.Text) > hora)
+            {
+                return false;
+            }else if (TbCusto.Text == "")
+            {
+                return false;
+            }else if (TbDistancia.Text == "")
+            {
+                return false;
+            }
+            else if (TbNivel.Text == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        //EXCLUIR UM REGISTRO DA TABELA 
+        private void excluir_Click(object sender, EventArgs e)
+        {
+            if (excluirItem == true)
+            {
+                int select = tabela.SelectedItems[0].Index;
+                var item = listItens[select].ItemArray;
+                try
+                {
+                    using (var cmd = new SQLiteCommand(DbConnection()))
+                    {
+                        cmd.CommandText = "DELETE FROM TB_VOO Where ID_VOO=@Id";
+                        cmd.Parameters.AddWithValue("@Id", int.Parse(item[0].ToString()));
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            limparCampos();
+            habilitar(false);
+            tabela.Items.Clear();
+            Listar();
+        }
+
+        //BOTÃO DE CANCELAR 
+        private void cancelar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+            habilitar(false);
+            excluirItem = false;
+            tabela.Items.Clear();
+            Listar();
+        }
+
+        private void checkBox_Click(object sender, EventArgs e)
+        {
+            checkBox2.CheckState = CheckState.Unchecked;
+        }
+
+        //HABILITAR E DESABILITAR OS CAMPOS DO FORMULARIO
+        private void habilitar(bool stats)
+        {
+            if (stats == true)
+            {
+                button3.Enabled = true;
+                button4.Enabled = true;
+                TbData.Enabled = true;
+                TbCusto.Enabled = true;
+                TbNivel.Enabled = true;
+                TbDistancia.Enabled = true;
+                checkBox1.Enabled = true;
+                checkBox2.Enabled = true;
+            }
+            else
+            {
+                button3.Enabled = false;
+                button4.Enabled = false;
+                TbData.Enabled = false;
+                TbCusto.Enabled = false;
+                TbNivel.Enabled = false;
+                TbDistancia.Enabled = false;
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = false;
+            }
+        }
+
+        private void checkBox2_Click(object sender, EventArgs e)
+        {
+            checkBox1.CheckState = CheckState.Unchecked;
+        }
+
+        //SELECIONAR UM ITEM DA LIST VIEW
+        private void tabela_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (tabela.SelectedItems.Count > 0)
+            {
+              int select = tabela.SelectedItems[0].Index;
+
+                var item = listItens[select].ItemArray;
+
+                TbData.Text = item[1].ToString();
+                TbCusto.Text = item[2].ToString();
+                TbDistancia.Text = item[3].ToString();
+                bool state = (item[4].ToString() == "N") ? false : true;
+                checkBox1.CheckState = (state == true) ? CheckState.Checked : CheckState.Unchecked;
+                checkBox2.CheckState = (state == false) ? CheckState.Checked : CheckState.Unchecked;
+                TbNivel.Text = item[5].ToString();
+
+                habilitar(true);
+                status = false;
+                excluirItem = true;
+            }
+        }
+
+        //BOTÃO INCLUIR 
+        private void incluir_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+            status = true;
+            habilitar(true);
+        }
+
+        //LIMPAR TODOS CAMPOS DO FORMULARIO
+        private void limparCampos()
+        {
+            TbData.Text = "";
+            TbCusto.Text = "";
+            TbDistancia.Text = "";
+            TbNivel.Text = "";
+            checkBox1.CheckState = CheckState.Unchecked;
+            checkBox2.CheckState = CheckState.Unchecked;
+            excluirItem = false;
+        }
+
+
+        //MASK PARA SOMENTE NÚMEROS NO CAMPO NIVEL
+        private void TbNivel_TextChanged_1(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(TbNivel.Text, "  ^ [0-9]"))
+            {
+                TbNivel.Text = "";
+            }
+        }
+        //MASK PARA SOMENTE NÚMEROS NO CAMPO NIVEL
+        private void TbNivel_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
